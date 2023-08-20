6891af5517a89620c24cbad16a2caf99ace9d15d
David Hewitt <davidmhewitt@gmail.com>
2023-08-20 15:00:00 +0100

    Handle event

--- lib/mapbox_phoenix_web/live/map_live.ex
+++ lib/mapbox_phoenix_web/live/map_live.ex
@@ -4,4 +4,10 @@ defmodule MapboxPhoenixWeb.MapLive do
   def mount(_params, _session, socket) do
     {:ok, socket}
   end
+
+  def handle_event("marker-moved", %{"coord" => [lng, lat]}, socket) do
+    IO.puts("Marker moved! Location: #{lng}, #{lat}")
+
+    {:noreply, socket}
+  end
 end
